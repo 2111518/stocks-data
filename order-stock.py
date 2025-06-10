8e4095725b04e2ca188117c7e518aa23318e029b
sun yi-wei <111208016@g.nccu.edu.tw>
2025-06-10 19:18:14 +0800

    0610 update

--- order-stock.py
+++ order-stock.py
@@ -14,7 +14,7 @@ def main():
     # 1. 使用者輸入股票代碼
     raw_input = input("請輸入股票代碼: ").strip()
     ticker_input = normalize_ticker(raw_input)
-
+    price = "Close"
     # 2. 使用者輸入開始與結束日期
     start_date_input = input("請輸入開始日期(YYYY-MM-DD): ").strip()
     end_date_input = input("請輸入結束日期(YYYY-MM-DD): ").strip()
@@ -37,18 +37,19 @@ def main():
     # 5. 抓取歷史收盤價
     hist = ticker.history(start=start_date_input, end=end_date_input)
     if hist.empty:
-        print("X 查無資料，請確認股票代碼與時間區間正確")
+        print("X 查無資料，請確認股票代碼與時間區間")
         return
 
     # 6. 整理成 CSV 格式
     df = pd.DataFrame({
         "股票名": ticker_input,
         "公司名": company_name,
-        "股價": hist["Close"].values,
+        "股價": hist[price].values,
         "日期": hist.index.strftime("%Y-%m-%d")
     })
     code = ticker_input.replace(".", "-")
-    filename = f"{code}-close-{start_date_input}-{end_date_input}.csv"
+    price_lower = price.lower()
+    filename = f"{code}-{price_lower}-{start_date_input}-{end_date_input}.csv"
     df.to_csv(filename, index=False)
     print(f"V 資料已保存")
     # print(f"V 資料已保存至 {filename}")
